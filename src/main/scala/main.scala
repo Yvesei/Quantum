a1c62a24318b14c48cae96e71a2eabccd007dca9
Nazih Ouchta <75317893+Yvesei@users.noreply.github.com>
2025-02-23 15:45:15 +0100

    TD4: Q1 fixed

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,22 +1,41 @@
 import Vqs._
 import Vqs.complex.QComplex
-import Vqs.operators.{$, <, C, H, Label, X, Y, Z}
+import Vqs.operators._
 
 object Main {
   QReg.setDefaultUseColor()
   QReg.setDefaultUseASCII()
 
   def main(args: Array[String]): Unit = {
-    val rr: QReg = QReg(3)
-    rr - $(0, "Bob") - $(1, "Alice") - $(2, "Phi")
+    // Create quantum register with 3 qubits
+    val rr = new QReg(3)
+
+    // Get random state
     val phiState: (QComplex, QComplex) = QUtils.randomState()
+
+    println(s"Alice (#1) and Bob (#0) share an EPR pair")
+    println(f"Alice has the QBit phi (#2) with the unknown state of\n ${phiState}")
+
+    // Set the state and continue with circuit
     rr.pokeQBitState(2, phiState)
+    println(rr)
+
+    println("Alice reads:")
+
     rr - H(0)
     rr - C(X(1), 0)
     rr - C(X(1), 2)
     rr - H(2)
-    rr - Y(0)
+    rr - <(1)- <(2)
+    rr - X(0)
     println(rr.render)
+
+
     println(rr)
+
+    rr.pokeQBitState(0, phiState)
+
+
+    println("\nBob State is : " + phiState)
   }
 }
